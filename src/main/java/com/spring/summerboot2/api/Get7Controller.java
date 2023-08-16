ce30c518895ed39fe2ab2a9fc58adbd4a5149c99
jeongm2n <wjdals8625@gmail.com>
2023-08-16 17:34:41 +0900

    일단 조금,,

--- src/main/java/com/spring/summerboot2/api/Get7Controller.java
+++ src/main/java/com/spring/summerboot2/api/Get7Controller.java
@@ -9,10 +9,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/api")
 public class Get7Controller {
 	
+	@RequestMapping(value = "/getWeather.do", method = RequestMethod.POST, produces = "application/json")
+	@ResponseBody
+	public List<WeatherVO> getWeather(@RequestParam("today") String today, @RequestParam("nx") String nx, @RequestParam("ny") String ny) throws Exception{
+		WeatherDAO wdao = new WeatherDAO();
+		
+		List<WeatherVO> weekWeather = wdao.get3Days(today, nx, ny);
+		
+		return weekWeather;
+	}
 }
