67eb93724b4a213d1bc6b703b52ee644050c3578
UnLuck <mirygm@naver.com>
2023-08-21 15:15:48 +0900

    결제기능 완성 무통장은 버튼만 있음

--- src/main/java/com/spring/summerboot2/pay/PayDAO.java
+++ src/main/java/com/spring/summerboot2/pay/PayDAO.java
@@ -3,9 +3,8 @@ package com.spring.summerboot2.pay;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
 import com.spring.summerboot2.DBconn;
 
@@ -15,12 +14,12 @@ public class PayDAO {
 	private Connection conn;
 	private ResultSet rs;
 	
-	public InformVO Load_Inform(String id, InformVO s_inform) {
+	public InformVO Load_Inform(String id, boolean s_inform) {
 		try {
 			InformVO inform = null;
 			conn = DBconn.getDBCP();
 
-			if(s_inform == null) {
+			if(!s_inform) {
 				String sql = "SELECT tel, mem_name, delivery_addr, point from sb_member where id='" + id + "'";
 				pstmt = conn.prepareStatement(sql);
 				rs = pstmt.executeQuery(sql);
@@ -44,8 +43,7 @@ public class PayDAO {
 				rs = pstmt.executeQuery(sql);
 				rs.next();
 				int point = rs.getInt("point");
-				
-				inform = s_inform;
+			
 				inform.setPoint(point);
 			}
 			
@@ -60,68 +58,24 @@ public class PayDAO {
 		return null;
 	}
 	
-	public String[] Add_Session(Map<String, String> Inform) {
-		String[] C_Inform = new String[2];
-		String tel = Inform.get("tel");
-		StringBuffer p_tel = new StringBuffer(tel);
-		p_tel.insert(7, "-"); p_tel.insert(3, "-");
-		C_Inform[0] = p_tel.toString();
-		String state = Inform.get("state");
-		String name = Inform.get("name");
-		String postcode = Inform.get("postcode");
-		String city = Inform.get("city");
-		String town = Inform.get("town");
-		String street_add = Inform.get("street_add");
-		String option_add = Inform.get("option_add");
-
-		C_Inform[1] = postcode + " ";
-		switch(state) {
-			case "강원특별자치도": case "제주특별자치도": case "세종특별자치시": C_Inform[1] += state + " "; break;
-			case "부산": case "대구": case "인천": case "광주": case "대전": case "울산": C_Inform[1] += state + "광역시 "; break;
-			case "경기": C_Inform[1] += state + "도 "; break;
-			case "서울": C_Inform[1] += state + "특별시 "; break;
-			case "충북": C_Inform[1] += "충청북도 "; break;
-			case "충남": C_Inform[1] += "충청남도 "; break;
-			case "전북": C_Inform[1] += "전라북도 "; break;
-			case "전남": C_Inform[1] += "전라남도 "; break;
-			case "경북": C_Inform[1] += "경상북도 "; break;
-			case "경남": C_Inform[1] += "경상남도 "; break;
-			default : break;
-		}
-		if(city.equals(null)) { }
-		else { C_Inform[1] += city + " ";} 	
-		C_Inform[1] += town + " ";
-		C_Inform[1] += street_add + " ";
-		C_Inform[1] += option_add;
-		
-		return C_Inform;
-	}
-	
-	public void Save_Inform(String id, Map<String, String> inform) {
+	public void Save_Inform(String id, String snssts, String state, String postcode, String city, String town,
+			String street_add, String option_add, String save_add) {
 		try {
 			conn = DBconn.getDBCP();
-
-			String snssts = inform.get("snssts");
 			
-			if(inform.get("save_add").equals("동의")) {
-				String state = inform.get("state");
-				String postcode = inform.get("postcode");
-				String city = inform.get("city");
-				String town = inform.get("town");
-				String street_add = inform.get("street_add");
-				String option_add = inform.get("option_add");
+			if(save_add.equals("동의")) {
 				
 				String address = postcode + "/" + state + "/" + city + "/" + town + "/" + street_add + "/" + option_add;
 				
 				String sql = "update sb_member set snssts = '" + snssts + "', delivery_addr = '" + address + "' where id = '" + id + "'";
 				pstmt = conn.prepareStatement(sql);
-				pstmt.executeUpdate(sql);
+				pstmt.executeUpdate();
 				
 			}
 			else {
 				String sql = "update sb_member set snssts = '" + snssts + "' where id = '" + id + "'";
 				pstmt = conn.prepareStatement(sql);
-				pstmt.executeUpdate(sql);
+				pstmt.executeUpdate();
 				
 			}
 			
@@ -133,6 +87,48 @@ public class PayDAO {
 			e.printStackTrace();
 		}
 		
+		
+		
+	}
+
+	public void pay_after(int point, String merchant_uid, String id, ArrayList<ProductVO> product, InformVO inform) {
+		try {
+			conn = DBconn.getDBCP();
+			LocalDate now = LocalDate.now();
+			
+			String address = inform.getPostcode() + inform.getState() + inform.getCity() + inform.getTown() + inform.getStreet_add() + inform.getOption_add();
+			String sql;
+			
+			for(ProductVO i_product : product) {
+				sql = "insert into sb_purchase (order_num, member_id, product_id, mount, address) values (?, ?, ?, ?, ?)";
+				pstmt = conn.prepareStatement(sql);
+				pstmt.setString(1, merchant_uid);
+				pstmt.setString(2, id);
+				pstmt.setInt(3, i_product.getProduct_id());
+				pstmt.setInt(4, i_product.getQuantity());
+				pstmt.setString(5, address);
+				pstmt.executeUpdate();
+			}
+			
+			sql = "delete from sb_cart where member_id = ?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, id);
+			pstmt.executeUpdate();
+			
+			if(point != 0) {
+				sql = "update sb_member set point = ? where id = ?";
+				pstmt = conn.prepareStatement(sql);
+				pstmt.setInt(1, inform.getPoint() - point);
+				pstmt.setString(2, id);
+				pstmt.executeUpdate();
+			}
+			
+			pstmt.close();
+			conn.close();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
 		
 	}
--- src/main/java/com/spring/summerboot2/pay/PayServiceimpl.java
+++ src/main/java/com/spring/summerboot2/pay/PayServiceimpl.java
@@ -1,27 +1,26 @@
 package com.spring.summerboot2.pay;
 
-import java.util.List;
-import java.util.Map;
+import java.util.ArrayList;
 
 import org.springframework.stereotype.Service;
 
 @Service
 public class PayServiceimpl implements PayService{
 	
-	public InformVO Load_Inform(String id, InformVO s_inform) {
+	public InformVO Load_Inform(String id,  boolean s_inform) {
 		PayDAO paydao = new PayDAO();
 		return paydao.Load_Inform(id, s_inform);
 	}
-
-	public String[] Add_Session(Map<String, String> inform) {
+	
+	public void Save_Inform(String id, String snssts, String state, String postcode, String city, String town,
+			String street_add, String option_add, String save_add) {
 		PayDAO paydao = new PayDAO();
-		return paydao.Add_Session(inform);
+		paydao.Save_Inform(id, snssts, state, postcode, city, town, street_add, option_add, save_add);
 	}
 	
-	public void Save_Inform(String id, Map<String, String> inform) {
+	public void pay_after(int point, String merchant_uid, String id, ArrayList<ProductVO> product, InformVO inform) {
 		PayDAO paydao = new PayDAO();
-		paydao.Save_Inform(id, inform);
+		paydao.pay_after(point, merchant_uid, id, product, inform);
 	}
-
 
 }
